a088add8e3e66c55173b65c5f54254d4cfce5b72
Josh Chappelow <jdchappelow@gmail.com>
2025-03-23 22:15:12 +0900

    wip: header

---
+++ app/components/header.tsx
@@ -0,0 +1,10 @@
+import Title from "antd/es/typography/Title";
+
+export default function Header() {
+  return (
+    <div style={{ textAlign: "center", marginBottom: "20px" }}>
+      <Title level={2}>NPM Package Downloads</Title>
+      <Title level={3}>Higher or Lower?</Title>
+    </div>
+  );
+}
